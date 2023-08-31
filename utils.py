385c3c0ead87386445cff8094fc396e3e7430334
Celshade <ggcelshade@gmail.com>
2023-08-30 23:28:21 -0400

    Create to_btc()

--- utils.py
+++ utils.py
@@ -8,3 +8,14 @@ SATS_IN_BTC = 100000000
 # PATHS
 ME_BASE_ADDR = "api-mainnet.magiceden.dev/v2/ord"
 ME_ACTIVITY_EP = "https://api-mainnet.magiceden.dev/v2/ord/btc/activities"
+
+
+# FUNCS
+def to_btc(sats: int) -> float:
+    """
+    Return the given sat value in bitcoin.
+
+    Args:
+        sats: The amount of sats to calculate.
+    """
+    return sats / SATS_IN_BTC
