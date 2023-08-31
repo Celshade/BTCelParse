e0c42f0cd09e50bd28da5cc6b116e13033d84bc0
Celshade <ggcelshade@gmail.com>
2023-08-30 23:21:43 -0400

    Add constant for SATS_IN_BTC

--- utils.py
+++ utils.py
@@ -2,6 +2,9 @@
 HEADERS = dict[str, str]
 ACTIVITY = dict[str, str | int]
 
+# CONSTANTS
+SATS_IN_BTC = 100000000
+
 # PATHS
 ME_BASE_ADDR = "api-mainnet.magiceden.dev/v2/ord"
 ME_ACTIVITY_EP = "https://api-mainnet.magiceden.dev/v2/ord/btc/activities"
